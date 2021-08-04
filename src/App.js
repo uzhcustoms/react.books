14eb56843e04a115817865206da902bcb6550775
uzhcustoms <uzhcustoms67@gmail.com>
2021-08-04 11:45:51 +0300

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+function Book(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="book-item">
+        <p>Название книги:</p>
+        <span>{props.title}</span>
+      </div>
+      <div className="book-item">
+        <p>Автор:</p>
+        <span>{props.author}</span>
+      </div>
+      <div className="book-item">
+        <p>Жанр:</p>
+        <span>{props.genre}</span>
+      </div>
+      <div className="book-item">
+        <p>К-ство страниц:</p>
+        <span>{props.pages}</span>
+      </div>
+      <div className="book-item">
+        <p>Рецензии:</p>
+        <div className="reviews">{props.reviews}</div>
+      </div>
     </div>
   );
+}
+
+
+function App() {
+  let books = {
+    qTitle: "Исток",
+    qAuthor: "Ден Браун",
+    qGenre: "детектив",
+    qPages: "577",
+    qReviews: ["Сюжет Браун завернул, как всегда, изысканно! Интрига сохранялась чуть ли не до последних страниц.",
+      "Удачно вплетённые в сюжет экскурсы в живопись, архитектуру, историю.", "На сегодняшний день «Исток» - это последняя книга о профессоре Лэнгдоне."
+    ]
+  };
+
+  let { qTitle, qAuthor, qGenre, qPages, qReviews } = books;
+
+  return (
+    <div className="App">
+      <h1>Любимая книга</h1>
+
+      <Book title={qTitle} author={qAuthor} genre={qGenre} pages={qPages} reviews={qReviews.map(review => (<p>{review}</p>))} />
+
+
+    </div>
+  )
 }
 
 export default App;
